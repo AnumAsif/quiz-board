a6799d958aa24035f99c3cde4a52351d35223ff7
Anum Asif <anum@cockar.com>
2018-12-10 14:44:57 +0300

    Add a function to check if all radio buttons are selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,44 @@
-$(document).ready(function(){
-  $("div.panel-heading").click(function(){
+$(document).ready(function() {
+  $("div.panel-heading").click(function() {
     $("div.options").hide();
-    var ansId = $(this).attr("id");
-    $("div#"+ansId+"-ans").toggle();
+    var answerId = $(this).attr("id");
+    $("div#" + answerId + "-ans").toggle();
   });
 
-  var correctAnswers = [2,1,3,3,2,2,4,2,4,2,3,1];
-
-
-
-
-  $("form").submit(function(event){
-    // window.location.replace("../quiz-board/index.html");
-    // $("#initialPanel").hide();
-    // $("#scorePanel").show();
-    $("form").hide();
-    $("#scorePanel").show();
+  var correctAnswers = [2, 1, 3, 3, 2, 2, 4, 2, 4, 2, 3, 1];
+  $("form").submit(function(event) {
     event.preventDefault();
-    var userAnswers = [];
-    var wrongAnswers=[];
-    var userScore=0;
-    for(var i=1;i<=correctAnswers.length; i++){
-    var userValue = $("input:radio[name=ans"+i+"]:checked").val();
-    userAnswers.push(userValue);
-    if(userValue == correctAnswers[i-1]){
-      userScore+=1;
-    }else{
-      wrongAnswers.push("Q"+i);
+    var blank = false;
+    $("input:radio").each(function() {
+    var val = $('input:radio[name=' + this.name + ']:checked').val();
+    if (val === undefined) {
+        blank = true;
+        return false;
     }
-  }
-  alert("userScore= "+userScore);
-  if(wrongAnswers.length == 0){
-    alert("no wrong answers")
-  }else{
-    alert("wrong answers= "+ wrongAnswers)
-  }
-  $("#score").text(userScore);
+    });
+    if(!blank){
+      $("form").hide();
+      $("#scorePanel").show();
+      var userAnswers = [];
+      var wrongAnswers = [];
+      var userScore = 0;
+      for (var i = 1; i <= correctAnswers.length; i++) {
+        var userValue = $("input:radio[name=ans" + i + "]:checked").val();
+        userAnswers.push(userValue);
+        if (userValue == correctAnswers[i - 1]) {
+          userScore += 1;
+        } else {
+          wrongAnswers.push("Q" + i);
+        }
+      }
+      $("#score").text(userScore+"/12");
+      if (wrongAnswers.length == 0) {
+        $("#wrongAnswersId").text("GREAT!!! No wrong answers");
+      } else {
+        $("#wrongAnswersId").text("Questions answered wrong are: "+wrongAnswers);
+      }
+    }else{
+      alert("Atleast one of the question is not answered");
+    }
   });
-
-
-
 });
-
-
-// $("div.panel-heading").click(function(event){
-//   var id =$(this).attr("id");
-//   $("div#"+id+"-def").toggle();
-// });
-// $("div.panel-body").click(function(){
-//
-//   $(this).hide();
-// });
